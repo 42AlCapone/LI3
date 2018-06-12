5963ed5e0159fbeaca9cca6cfb7128a0c77d0bba
asergiogomes <ae3654@alunos.uminho.pt>
2018-06-12 02:11:33 +0100

    tags.java

--- proj-java/src/main/java/engine/Tag.java
+++ proj-java/src/main/java/engine/Tag.java
@@ -1,3 +1,12 @@
+/**
+ * Classe expecífica de contagem de utilizações de tags.
+ *
+ * @author António Gomes
+ * @author João Vieira
+ * @author Rui Vieira
+ * @version 1.1
+ */
+
 package engine;
 
 import java.io.Serializable;
@@ -8,15 +17,37 @@ import java.util.HashMap;
 public class Tag implements Serializable {
 	private Long tagUses;
 	
-
+	/**
+	 * Construtor por omissão da classe <code>Tag</code>
+	 * <p>
+	 * Iguala <code>tagUses</code> a 0 em <code>Long</code>
+	 *
+	 * @see java.lang.Long
+	 */
 	public Tag() {
 		this.tagUses = 0L;
 		}
 
+	/**
+	 * Construtor parametrizado da classe <code>Tag</code>
+	 * <p>
+	 * Iguala <code>tagUses</code> a um argumento <code>Long tagUses</code>
+	 *
+	 * @see java.lang.Long
+	 */
 	public Tag(Long tagUses) {
 		this.tagUses = tagUses;
 		}
 
+	/**
+	 * Construtor por cópia da classe <code>Tag</code>
+	 * <p>
+	 * Iguala <code>tagUses</code> a <code>tagUses</code> do objecto <code>Tag tag</code> passado
+	 * por argutmento atravéz do uso do método getTagUses();
+	 * 
+	 * @see #getTagUses()
+	 * @see java.lang.Long
+	 */
 	public Tag(Tag tag) {
 		this.tagUses = tag.getTagUses();
 		}
@@ -24,6 +55,11 @@ public class Tag implements Serializable {
 
 	// Getters
 
+	/**
+	 * Método de acesso a <code>tagUses</code>.
+	 * 
+	 * @return As tagUses do objecto associado.
+	 */
 	public Long getTagUses() {
 		return this.tagUses;
 	}
@@ -31,15 +67,36 @@ public class Tag implements Serializable {
 
 	// Setters
 
+	/**
+	 * Método de escrita das <code>tagUses</code>.
+	 * <p>
+	 * Iguala as <code>tagUses</code> a um <code>Long uses</code> passado por argumento.
+	 *
+	 * @see java.lang.Long
+	 */
 	public void setTagUses(Long uses) {
 		this.tagUses = uses;
 	}
 
+	/**
+	 * Método de incremento das <code>tagUses</code>
+	 * <p>
+	 * Incrementa as <code>tagUses</code> por um.
+	 */
 	public void incTagUse() {
 		this.tagUses++;
 	}
 	
-
+	/**
+	 * Método de comparação entre um objecto e uma <code>Tag</code> (tag use).
+	 * @return true se <code>obj</code> for igual à tag associada
+	 * @return true caso <code>obj</code> seja uma <code>Tag</code> não nula e caso as 
+	 * <code>tagUses</code> do objecto forem iguais às tagUses da <code>Tag</code> associada.
+	 * @return false caso a comparação das <code>tagUses</code> falhar.
+	 * @return false caso <code>obj</code> for nulo.
+	 * @return false caso <code>obj</code> não pertença à classe <code>Tag</code>
+	 * @see java.lang.Object#getClass()
+	 */
 	public boolean equals(Object obj) {
 		if (obj == this)
 			return true;
@@ -50,6 +107,15 @@ public class Tag implements Serializable {
 		return t.getTagUses() == this.tagUses;
 		}
 
+	/**
+	 * Método de cópia da classe <code>Tag</code>.
+	 * <p>
+	 * Atravéz da utilização do construtor por cópia <code>Tag(Tag)</code>
+	 * este método retorna uma nova tag que é uma exata cópia da <code>Tag</code> associada.
+	 *
+	 * @return Uma <code>Tag</code> que é uma cópia da <code>Tag</code> associada.
+	 * @see #Tag(Tag)
+	 */
 	public Tag clone() {
 		return new Tag(this);
 	}
